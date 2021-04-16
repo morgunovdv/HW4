2f6706fa8da3aea36eb61507b3ceba0f1a8951d8
Денис Моргунов <Morgunovdv3@hotmail.com>
2021-04-16 17:06:39 +0700

    1

---
+++ StringSearcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
