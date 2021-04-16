7fef101d1ea09eddaef33c8a7e858712f6ee1729
Денис Моргунов <Morgunovdv3@hotmail.com>
2021-04-16 17:07:18 +0700

    1

--- Counter/Handler2.cs
+++ Counter/Handler2.cs
@@ -8,12 +8,6 @@ namespace Counter
 {
     class Handler2
     {
-        public delegate void Message();
-
-        Message mes = Mess;
-
-        
-
         public static void Mess()
         {
             Console.WriteLine("Уже 77, давно пора было начать!");
